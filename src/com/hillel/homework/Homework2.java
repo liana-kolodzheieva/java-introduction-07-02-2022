459bcbba6a56349d7f2bcec4087a2669ccdfcbfe
Liana Kolodzheieva <liana.kolodgeeva@gmail.com>
2022-02-10 21:47:38 +0300

    add Homework2

---
+++ src/com/hillel/homework/Homework2.java
@@ -0,0 +1,13 @@
+package com.hillel.homework;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        double longitude = 46.48725432457797;
+        double latitude = 30.638289185025364;
+        System.out.print("мое местоположение");
+        System.out.print(longitude + " ");
+        System.out.println(latitude);
+    }
+
+}
+
